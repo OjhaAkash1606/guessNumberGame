e499e75475aa9c80faef29ea8ee388243fe66fd9
OjhaAkash1606 <ojha.akash876@gmail.com>
2021-12-26 12:39:43 +0530

    Complete game

---
+++ guess.js
@@ -0,0 +1,98 @@
+const questionSymbol = document.querySelector(".questionMark");
+const checkButton = document.querySelector(".check");
+const messageDisplay = document.querySelector(".message");
+const scoringDisplay = document.querySelector(".scoring");
+const bodyDisplay = document.querySelector("body");
+const highScoringDisplay = document.querySelector(".highScoring");
+const againButton = document.querySelector(".again");
+const secretNumberInvisible = document.querySelector(".secret");
+const guessNumber = document.querySelector(".guess");
+
+let secretNumber = Math.ceil(Math.random() * 20);
+questionSymbol.textContent = secretNumber;
+
+let scoreNumber = 10;
+scoringDisplay.textContent = scoreNumber;
+let highScoreNumber = 0;
+
+highScoringDisplay.textContent = "0";
+
+checkButton.addEventListener("click", () => {
+  const guessNumberDisplay = Number(guessNumber.value);
+
+  if (guessNumberDisplay === secretNumber) {
+    if (scoreNumber === 10) {
+      messageDisplay.textContent = "You win the game!";
+      guessNumber.setAttribute("disabled", "true");
+      bodyDisplay.style.backgroundColor = "#87CEEB";
+      guessNumber.style.backgroundColor = "aliceblue";
+      guessNumber.style.color = "black";
+      highScoreNumber = scoreNumber;
+      highScoringDisplay.textContent = highScoreNumber;
+    } else {
+      messageDisplay.textContent = "Correct Number!";
+      guessNumber.setAttribute("disabled", "true");
+      bodyDisplay.style.backgroundColor = "#87CEEB";
+      guessNumber.style.backgroundColor = "aliceblue";
+      guessNumber.style.color = "black";
+      highScoreNumber = scoreNumber;
+      highScoringDisplay.textContent = highScoreNumber;
+    }
+  }
+  if (guessNumberDisplay > secretNumber && scoreNumber > 0) {
+    if (guessNumberDisplay > 20) {
+      messageDisplay.textContent = "Invalid Number!";
+    } else if (guessNumberDisplay > secretNumber) {
+      messageDisplay.textContent = "Too High!";
+      scoreNumber--;
+      scoringDisplay.textContent = scoreNumber;
+      if (scoreNumber === 0) {
+        messageDisplay.textContent = "You lost the game!";
+        guessNumber.setAttribute("disabled", "true");
+        bodyDisplay.style.backgroundColor = "#87CEEB";
+        guessNumber.style.backgroundColor = "aliceblue";
+        guessNumber.style.color = "black";
+        scoringDisplay.textContent = 0;
+      }
+    }
+  }
+  if (guessNumberDisplay < secretNumber && scoreNumber > 0) {
+    if (guessNumberDisplay < 1) {
+      messageDisplay.textContent = "Invalid Number!";
+    } else if (guessNumberDisplay < secretNumber) {
+      messageDisplay.textContent = "Too Low!";
+      scoreNumber--;
+      scoringDisplay.textContent = scoreNumber;
+      if (scoreNumber === 0) {
+        messageDisplay.textContent = "You lost the game!";
+        guessNumber.setAttribute("disabled", "true");
+        bodyDisplay.style.backgroundColor = "#87CEEB";
+        guessNumber.style.backgroundColor = "aliceblue";
+        guessNumber.style.color = "black";
+        scoringDisplay.textContent = 0;
+      }
+    }
+  }
+});
+againButton.addEventListener("click", () => {
+  const guessNumberDisplay = Number(guessNumber.value);
+  if (guessNumberDisplay === secretNumber) {
+    if (scoreNumber === 10) {
+      guessNumber.setAttribute("disabled", "true");
+      bodyDisplay.style.backgroundColor = "#87CEEB";
+      messageDisplay.textContent = "You win the game!";
+      highScoringDisplay.textContent = highScoreNumber;
+      scoringDisplay.textContent = scoreNumber;
+    } else {
+      guessNumber.value = "0";
+      guessNumber.removeAttribute("disabled", "true");
+      secretNumber = Math.ceil(Math.random() * 20);
+      secretNumberInvisible.textContent = secretNumber;
+      bodyDisplay.style.backgroundColor = "black";
+      messageDisplay.textContent = "Start guessing...!";
+      highScoringDisplay.textContent = highScoreNumber;
+      scoreNumber = 10;
+      scoringDisplay.textContent = scoreNumber;
+    }
+  }
+});
